e25b19f078281657c946a0ea4aa7f7c8104733cf
chetan-parmar <cparmar613@gmail.com>
2023-12-04 18:57:01 +0530

    Update Vendor model, add Purchase Order model and Historical Performance model

--- vendor_management_system/app/admin.py
+++ vendor_management_system/app/admin.py
@@ -1,7 +1,19 @@
+# admin.py
+
 from django.contrib import admin
-from .models import Vendor
+from .models import Vendor, PurchaseOrder, HistoricalPerformance
 
 @admin.register(Vendor)
 class VendorAdmin(admin.ModelAdmin):
-    list_display = ('name', 'contact_details', 'vendor_code')
+    list_display = ('name', 'contact_details', 'vendor_code', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate')
     search_fields = ('name', 'vendor_code')
+
+@admin.register(PurchaseOrder)
+class PurchaseOrderAdmin(admin.ModelAdmin):
+    list_display = ('po_number', 'vendor', 'order_date', 'delivery_date', 'status', 'quality_rating', 'issue_date', 'acknowledgment_date')
+    search_fields = ('po_number', 'vendor__name')
+
+@admin.register(HistoricalPerformance)
+class HistoricalPerformanceAdmin(admin.ModelAdmin):
+    list_display = ('vendor', 'date', 'on_time_delivery_rate', 'quality_rating_avg', 'average_response_time', 'fulfillment_rate')
+    search_fields = ('vendor__name', 'date')
--- vendor_management_system/app/models.py
+++ vendor_management_system/app/models.py
@@ -1,10 +1,51 @@
 from django.db import models
 
 class Vendor(models.Model):
+    """
+    Model for storing vendor information and performance metrics.
+    """
     name = models.CharField(max_length=255)
     contact_details = models.CharField(max_length=255)
     address = models.TextField()
     vendor_code = models.CharField(max_length=20, unique=True)
+    on_time_delivery_rate = models.FloatField(null=True, blank=True)
+    quality_rating_avg = models.FloatField(null=True, blank=True)
+    average_response_time = models.FloatField(null=True, blank=True)
+    fulfillment_rate = models.FloatField(null=True, blank=True)
 
     def __str__(self):
         return self.name
+    
+
+class PurchaseOrder(models.Model):
+    """
+    Model for capturing details of each purchase order.
+    """
+    vendor = models.ForeignKey('Vendor', on_delete=models.CASCADE)
+    po_number = models.CharField(max_length=20, unique=True)
+    order_date = models.DateTimeField()
+    delivery_date = models.DateTimeField()
+    items = models.JSONField()
+    quantity = models.IntegerField()
+    status = models.CharField(max_length=20)
+    quality_rating = models.FloatField(null=True, blank=True)
+    issue_date = models.DateTimeField()
+    acknowledgment_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return f"PO {self.po_number} - {self.vendor.name}"
+    
+
+class HistoricalPerformance(models.Model):
+    """
+    Model for storing historical data on vendor performance.
+    """
+    vendor = models.ForeignKey('Vendor', on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    on_time_delivery_rate = models.FloatField()
+    quality_rating_avg = models.FloatField()
+    average_response_time = models.FloatField()
+    fulfillment_rate = models.FloatField()
+
+    def __str__(self):
+        return f"Historical Performance - {self.vendor.name} - {self.date}"
